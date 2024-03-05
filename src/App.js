0cb108b890ce047f39d793ac7c4e51c17aa280c6
MuhammadAsifHossain <141495638+MuhammadAsifHossain@users.noreply.github.com>
2024-03-05 22:30:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useState } from "react";
+const App = () => {
+  const [Value, setValue] = useState(0);
+  function CountClick() {
+    setValue(Value + 1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={CountClick}>Count Clicks</button>
+      <p>Click number : {Value}</p>
     </div>
   );
-}
-
+};
 export default App;
