7d731373127f3196dbbfbe8163c32bed275b4ced
MuhammadAsifHossain <141495638+MuhammadAsifHossain@users.noreply.github.com>
2024-03-13 13:27:53 +0530

    modifying the code to a simple project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
+//step 1 importing State
 import { useState } from "react";
+// creating a function to return random animal to an array
+const AddRandomAnimal = () => {
+  const animals = ["Lion", "Tiger", "Leopard", "Jaguar", "Cheetah"];
+  return animals[Math.floor(Math.random() * animals.length)];
+  //Math.random() generates a random number and Math.floor() rounds a number to the nearest integer
+};
+
+//step 2: creating the component in order to export to index.js
 const App = () => {
-  const [Value, setValue] = useState(0);
-  function CountClick() {
-    setValue(Value + 1);
-  }
+  // step 4: initializing state with an empty array in order to add values
+  const [animal, setanimal] = useState([]);
+  //step 4 putting values into the state using Event function and state concet
+  const ButtonClick = () => {
+    /* STEP 5: ... Updates Arrays with State: In React components, 
+    this pattern is often used to create a new array based on an existing state variable 
+    and potentially some dynamic data from a function call (here AddRandomAnimal).*/
+    setanimal([...animal, AddRandomAnimal()]);
+  };
+
   return (
+    //Step 6: using onClick Event with ButtonClick function we use {} here
+    //and not call a function because it will call the fucntion even if button is not clicked
     <div>
-      <button onClick={CountClick}>Count Clicks</button>
-      <p>Click number : {Value}</p>
+      <button onClick={ButtonClick}>Add Animal</button>
+      <div>{animal}</div>
     </div>
   );
+  // step 7:to show on screen the randomly generated value {} the state value
 };
+//step 3 exporting App component as default export
 export default App;
